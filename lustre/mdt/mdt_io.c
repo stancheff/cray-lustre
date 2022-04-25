ff1f0438a565fb870b2ef610b862c6a84b8296a0
Mikhail Pershin <mpershin@whamcloud.com>
2022-04-25 09:13:53 +0300

    LU-12031 mdt: explicit data version of DoM files
    Use EA to store 'data_version' for DoM files explicitly.
    
    Unlike OST objects the 'inode_version' of DoM file is changed
    by metadata operations as well and that leads to problems
    during HSM operations, e.g. writing HSM EA with file data
    version inside causes DoM object version update making this
    HSM EA version obsoleted, also any metadata update on
    restored file makes it dirty and prevents second release.
    
    DoM files have now explicitly updated 'data_version' in
    addition to ordinary 'inode_version'. The 'data_version'
    is updated along with 'inode_version' upon write/truncate and
    fallocate operations and is stored as 'trusted.dataver' EA.
    Layout swap procedure is updated to move data version between
    files being swept along with HSM attributes.
    If DoM file is migrated to RAID0 file then 'dataver' EA is
    deleted.
    
    Corresponding test 1f is added to sanity-hsm.sh and
    270j to sanity.sh.
    
    Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
    Change-Id: I4689c56394c7323d32cd6f7dd86f58beb6e53353
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/47139
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162436
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

--- lustre/mdt/mdt_io.c
+++ lustre/mdt/mdt_io.c
@@ -650,7 +650,7 @@ retry:
 			GOTO(out_stop, rc);
 	}
 
-	tgt_vbr_obj_set(env, dob);
+	tgt_vbr_obj_data_set(env, dob, true);
 	rc = dt_trans_start(env, dt, th);
 	if (rc)
 		GOTO(out_stop, rc);
@@ -904,7 +904,7 @@ int mdt_object_fallocate(const struct lu_env *env, struct dt_device *dt,
 	if (rc)
 		GOTO(stop, rc);
 
-	tgt_vbr_obj_set(env, dob);
+	tgt_vbr_obj_data_set(env, dob, true);
 	rc = dt_trans_start(env, dt, th);
 	if (rc)
 		GOTO(stop, rc);
@@ -1094,7 +1094,7 @@ int mdt_object_punch(const struct lu_env *env, struct dt_device *dt,
 	if (rc)
 		GOTO(stop, rc);
 
-	tgt_vbr_obj_set(env, dob);
+	tgt_vbr_obj_data_set(env, dob, true);
 	rc = dt_trans_start(env, dt, th);
 	if (rc)
 		GOTO(stop, rc);
@@ -1645,7 +1645,7 @@ int mdt_data_version_get(struct tgt_session_info *tsi)
 		GOTO(out, rc = -EBADF);
 
 	/* Get version first */
-	version = dt_version_get(tsi->tsi_env, mdt_obj2dt(mo));
+	version = dt_data_version_get(tsi->tsi_env, mdt_obj2dt(mo));
 	if (version && version != -EOPNOTSUPP) {
 		repbody->mbo_valid |= OBD_MD_FLDATAVERSION;
 		/* re-use mbo_ioepoch to transfer version */
