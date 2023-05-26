Cray/lustre

commit 5e448f222261305549ba772fe86c7ce162189457

LUS-11330 lod: check the inherited pool for conflicts
In addition to LU-15658, the start index could be inherited from
parent and the pool from root: drop the pool in case of conflict
as well.

Also, let the stripe_count to be adjusted by the count of targets
in the pool only if the pool is not dropped.

Another case of a problem inheritance is saving the inherited LOVEA
to subdir, when all the parameters are inherited but the ost list.

Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: Ief1dbd8c1ee0433bb625cbff1834b248d4fb2992
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/161800
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andriy Skulysh <c17819@cray.com>

lustre/lod/lod_object.c
@@ -2707,8 +2707,10 @@ __u16 lod_comp_entry_stripe_count(struct lod_object *lo,
 	struct lod_device *lod = lu2lod_dev(lod2lu_obj(lo)->lo_dev);
 	struct lod_layout_component *entry;
 
-	if (is_dir)
-		return  0;
+	if (is_dir) {
+		entry = &lo->ldo_def_striping->lds_def_comp_entries[comp_idx];
+		return entry->llc_ostlist.op_count;
+	}
 
 	entry = &lo->ldo_comp_entries[comp_idx];
 	if (lod_comp_inited(entry))
@@ -5899,7 +5901,7 @@ static void lod_ah_init(const struct lu_env *env,
 				lod_comp->llc_stripe_offset =
 					def_comp->llc_stripe_offset;
 			if (lod_comp->llc_pool == NULL)
-				lod_obj_set_pool(lc, 0, def_comp->llc_pool);
+				lod_qos_set_pool(lc, 0, def_comp->llc_pool);
 		}
 	}
 out:
@@ -5923,7 +5925,7 @@ out:
 		desc = &d->lod_ost_descs.ltd_lov_desc;
 		lod_adjust_stripe_info(lod_comp, desc, ah->dah_append_stripes);
 		if (ah->dah_append_pool && ah->dah_append_pool[0])
-			lod_obj_set_pool(lc, 0, ah->dah_append_pool);
+			lod_qos_set_pool(lc, 0, ah->dah_append_pool);
 	}
 
 	EXIT;

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -1241,11 +1241,11 @@ static inline __u32 lov_mds_md_size(__u16 stripes, __u32 lmm_magic)
 	if (stripes == (__u16)-1)
 		stripes = 0;
 
-	if (lmm_magic == LOV_MAGIC_V3)
-		return sizeof(struct lov_mds_md_v3) +
-				stripes * sizeof(struct lov_ost_data_v1);
-	else
+	if (lmm_magic == LOV_MAGIC_V1)
 		return sizeof(struct lov_mds_md_v1) +
+				stripes * sizeof(struct lov_ost_data_v1);
+	else /* LOV_MAGIC_V3 and LOV_MAGIC_SPECIFIC */
+		return sizeof(struct lov_mds_md_v3) +
 				stripes * sizeof(struct lov_ost_data_v1);
 }
 

lustre/lod/lod_internal.h
@@ -734,6 +734,7 @@ int lod_use_defined_striping(const struct lu_env *, struct lod_object *,
 			     const struct lu_buf *);
 int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 			 const struct lu_buf *buf);
+void lod_qos_set_pool(struct lod_object *lo, int pos, char *pool_name);
 int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 			struct lu_attr *attr, struct thandle *th,
 			int comp_idx, __u64 reserve);

lustre/lod/lod_lov.c
@@ -732,6 +732,9 @@ static int lod_gen_component_ea(const struct lu_env *env,
 		lod_comp = &lo->ldo_comp_entries[comp_idx];
 
 	magic = lod_comp->llc_pool != NULL ? LOV_MAGIC_V3 : LOV_MAGIC_V1;
+	if (is_dir && lod_comp->llc_ostlist.op_count)
+		magic = LOV_MAGIC_SPECIFIC;
+
 	if (lod_comp->llc_pattern == 0) /* default striping */
 		lod_comp->llc_pattern = LOV_PATTERN_RAID0;
 
@@ -758,7 +761,7 @@ static int lod_gen_component_ea(const struct lu_env *env,
 	} else {
 		struct lov_mds_md_v3 *v3 = (struct lov_mds_md_v3 *)lmm;
 		size_t cplen = strlcpy(v3->lmm_pool_name,
-				       lod_comp->llc_pool,
+				       lod_comp->llc_pool ? : "\0",
 				       sizeof(v3->lmm_pool_name));
 		if (cplen >= sizeof(v3->lmm_pool_name))
 			RETURN(-E2BIG);
@@ -780,7 +783,8 @@ static int lod_gen_component_ea(const struct lu_env *env,
 	if (!is_dir && lo->ldo_is_composite)
 		lod_comp_shrink_stripe_count(lod_comp, &stripe_count);
 
-	if (is_dir || lod_comp->llc_pattern & LOV_PATTERN_F_RELEASED)
+	if ((is_dir && magic != LOV_MAGIC_SPECIFIC) ||
+	    lod_comp->llc_pattern & LOV_PATTERN_F_RELEASED)
 		GOTO(done, rc = 0);
 
 	/* generate ost_idx of this component stripe */

lustre/lod/lod_qos.c
@@ -2140,8 +2140,7 @@ unlock:
 	RETURN(rc);
 }
 
-static void lod_qos_set_pool(struct lod_object *lo, int pos, char *pool_name,
-			     struct lov_user_md_v1 *v1)
+void lod_qos_set_pool(struct lod_object *lo, int pos, char *pool_name)
 {
 	struct lod_device *d = lu2lod_dev(lod2lu_obj(lo)->lo_dev);
 	struct lod_layout_component *lod_comp;
@@ -2157,11 +2156,8 @@ static void lod_qos_set_pool(struct lod_object *lo, int pos, char *pool_name,
 	if (pool) {
 		lod_comp = &lo->ldo_comp_entries[pos];
 		if (lod_comp->llc_stripe_offset != LOV_OFFSET_DEFAULT) {
-			if (v1->lmm_magic == LOV_USER_MAGIC_SPECIFIC) {
-				struct lov_user_md_v3 *v3;
-
-				v3 = (struct lov_user_md_v3 *)v1;
-				for (j = 0; j < v3->lmm_stripe_count; j++) {
+			if (lod_comp->llc_ostlist.op_count) {
+				for (j = 0; j < lod_comp->llc_ostlist.op_count; j++) {
 					__u32 num;
 
 					num = lod_comp->llc_ostlist.op_array[j];
@@ -2183,7 +2179,8 @@ static void lod_qos_set_pool(struct lod_object *lo, int pos, char *pool_name,
 			}
 		}
 
-		if (lod_comp->llc_stripe_count > pool_tgt_count(pool) &&
+		if (pool_name &&
+		    lod_comp->llc_stripe_count > pool_tgt_count(pool) &&
 		    !(lod_comp->llc_pattern & LOV_PATTERN_OVERSTRIPING))
 			lod_comp->llc_stripe_count = pool_tgt_count(pool);
 
@@ -2391,7 +2388,7 @@ int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 		}
 
 		lod_comp->llc_stripe_offset = v1->lmm_stripe_offset;
-		lod_qos_set_pool(lo, i, pool_name, v1);
+		lod_qos_set_pool(lo, i, pool_name);
 	}
 
 	RETURN(0);

stancheff/cray-lustre

commit 7baa9efe02a6a4025ed018920552b381f0cef304

LUS-11631 lod: fix lovea size
LOVEA size calculation for composite layouts became different in
lod_comp_md_size() and lod_gen_component_ea() after LUS-11330.

Fixes: 5e448f2222 ("LUS-11330 lod: check the inherited pool for conflicts")
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: I2dd93cded2c9d1f484d731528594a3cfbdc79035
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162396
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

lustre/lod/lod_object.c
@@ -2761,6 +2761,8 @@ static int lod_comp_md_size(struct lod_object *lo, bool is_dir)
 		if (!is_dir && is_composite)
 			lod_comp_shrink_stripe_count(&comp_entries[i],
 						     &stripe_count);
+		if (is_dir && comp_entries[i].llc_ostlist.op_count)
+			magic = LOV_MAGIC_SPECIFIC;
 
 		size += lov_user_md_size(stripe_count, magic);
 		LASSERT(size % sizeof(__u64) == 0);