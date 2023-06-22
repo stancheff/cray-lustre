Cray/lustre

commit 00c3908e62939c666bb039ee4a0da642c2631385

LU-16655 scrub: upgrade scrub_file from 2.12 format
Scrub_file->sf_oi_count has different offsets in Lustre-2.10,
Lustre-2.12, and Lustre-2.15 due to unintended format changes.
Lustre-2.15 reads sf_oi_count from offset of sf_success_count
and may initialize incorrect number of OI files, and not be
able to do FID lookups for existing filesystem objects.

Fixes: a114f6b8c5 ("LU-13344 servers: change request timeouts to s32")
Fixes: 4c2f028a95 ("LU-9019 osd-ldiskfs: migrate to 64 bit time")
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: Id7c8bd555229405d604456c48447f01fd121aca9
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50455
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162341
Tested-by: Jenkins Build User <nssreleng@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>

lustre/obdclass/scrub.c
@@ -119,7 +119,7 @@ void scrub_file_init(struct lustre_scrub *scrub, uuid_t uuid)
 
 	memset(sf, 0, sizeof(*sf));
 	uuid_copy(&sf->sf_uuid, &uuid);
-	sf->sf_magic = SCRUB_MAGIC_V1;
+	sf->sf_magic = SCRUB_MAGIC_V2;
 	sf->sf_status = SS_INIT;
 }
 EXPORT_SYMBOL(scrub_file_init);
@@ -133,6 +133,7 @@ void scrub_file_reset(struct lustre_scrub *scrub, uuid_t uuid, u64 flags)
 	       scrub->os_name, sf->sf_flags, flags);
 
 	uuid_copy(&sf->sf_uuid, &uuid);
+	sf->sf_magic = SCRUB_MAGIC_V2;
 	sf->sf_status = SS_INIT;
 	sf->sf_flags |= flags;
 	sf->sf_flags &= ~SF_AUTO;
@@ -183,9 +184,13 @@ int scrub_file_load(const struct lu_env *env, struct lustre_scrub *scrub)
 	}
 
 	scrub_file_to_cpu(sf, &scrub->os_file_disk);
-	if (sf->sf_magic != SCRUB_MAGIC_V1) {
-		CDEBUG(D_LFSCK, "%s: invalid scrub magic 0x%x != 0x%x\n",
-		       scrub->os_name, sf->sf_magic, SCRUB_MAGIC_V1);
+	if (sf->sf_magic == SCRUB_MAGIC_V1) {
+		CWARN("%s: reset scrub OI count for format change (LU-16655)\n",
+		      scrub->os_name);
+		sf->sf_oi_count = 0;
+	} else if (sf->sf_magic != SCRUB_MAGIC_V2) {
+		CDEBUG(D_LFSCK, "%s: invalid scrub magic %#x, should be %#x\n",
+		       scrub->os_name, sf->sf_magic, SCRUB_MAGIC_V2);
 		return -EFAULT;
 	}
 

lustre/osd-ldiskfs/osd_oi.c
@@ -397,8 +397,9 @@ int osd_oi_init(struct osd_thread_info *info, struct osd_device *osd,
 	int rc;
 	ENTRY;
 
-	if (unlikely(sf->sf_oi_count & (sf->sf_oi_count - 1)) != 0) {
-		LCONSOLE_WARN("%s: Invalid OI count in scrub file %d\n",
+	if (unlikely((sf->sf_oi_count & (sf->sf_oi_count - 1)) != 0 ||
+		     sf->sf_oi_count > OSD_OI_FID_NR_MAX)) {
+		LCONSOLE_WARN("%s: invalid OI count %u in scrub file, reset it\n",
 			      osd_dev2name(osd), sf->sf_oi_count);
 		sf->sf_oi_count = 0;
 	}

lustre/include/lustre_scrub.h
@@ -44,6 +44,7 @@
 #define PFID_STRIPE_COUNT_MASK	((1 << PFID_STRIPE_IDX_BITS) - 1)
 
 #define SCRUB_MAGIC_V1			0x4C5FD252
+#define SCRUB_MAGIC_V2			0x4C5FE253
 #define SCRUB_CHECKPOINT_INTERVAL	60
 #define SCRUB_WINDOW_SIZE		1024
 

lustre/osd-zfs/osd_scrub.c
@@ -1380,8 +1380,9 @@ int osd_scrub_setup(const struct lu_env *env, struct osd_device *dev,
 			dirty = true;
 		}
 
-		if ((sf->sf_oi_count & (sf->sf_oi_count - 1)) != 0) {
-			LCONSOLE_WARN("%s: invalid oi count %d, set it to %d\n",
+		if (unlikely((sf->sf_oi_count & (sf->sf_oi_count - 1)) != 0 ||
+			     sf->sf_oi_count > OSD_OI_FID_NR_MAX)) {
+			LCONSOLE_WARN("%s: invalid OI count %u, reset to %u\n",
 				      osd_name(dev), sf->sf_oi_count,
 				      osd_oi_count);
 			sf->sf_oi_count = osd_oi_count;

stancheff/cray-lustre

commit 8dfb75fa597ace9f603651c5f63be2afe588e832

LUS-11646 osd: update OI_Scrub file with new magic
The fix for LUS-11542 detects the format change correctly
but does not write new oi scrub file magic, so new mount
triggers the "oi files counter reset" again and again.

Fixes: 00c3908e62 ("LU-16655 scrub: upgrade scrub_file from 2.12 format")
Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Change-Id: Ia13fcfaf0d8f2c4ee9331dd9fec0ff159d195186
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162405
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

lustre/obdclass/scrub.c
@@ -127,6 +127,7 @@ EXPORT_SYMBOL(scrub_file_init);
 void scrub_file_reset(struct lustre_scrub *scrub, uuid_t uuid, u64 flags)
 {
 	struct scrub_file *sf = &scrub->os_file;
+	ENTRY;
 
 	CDEBUG(D_LFSCK, "%s: reset OI scrub file, old flags = "
 	       "%#llx, add flags = %#llx\n",
@@ -150,6 +151,7 @@ void scrub_file_reset(struct lustre_scrub *scrub, uuid_t uuid, u64 flags)
 	sf->sf_items_igif = 0;
 	if (!scrub->os_in_join)
 		sf->sf_items_updated_prior = 0;
+	EXIT;
 }
 EXPORT_SYMBOL(scrub_file_reset);
 

lustre/osd-ldiskfs/osd_oi.c
@@ -316,7 +316,7 @@ osd_oi_table_open(struct osd_thread_info *info, struct osd_device *osd,
 
 		if (rc == -ENOENT && create == false) {
 			if (oi_count == 0)
-				return count;
+				RETURN(count);
 
 			rc = 0;
 			ldiskfs_set_bit(i, sf->sf_oi_bitmap);
@@ -425,23 +425,20 @@ int osd_oi_init(struct osd_thread_info *info, struct osd_device *osd,
 		if (count == sf->sf_oi_count)
 			GOTO(out, rc = count);
 
-		if (sf->sf_oi_count == 0) {
-			if (likely((count & (count - 1)) == 0))
-				GOTO(out, rc = count);
-
-			LCONSOLE_WARN(
-				      "%s: invalid oi count %d, remove them, then set it to %d\n",
+		/* Trust the counted number of OI files if it is sane */
+		if ((count & (count - 1)) != 0) {
+			LCONSOLE_WARN("%s: invalid oi count %d, remove them, then set it to %d\n",
 				      osd_dev2name(osd), count, osd_oi_count);
 			osd_oi_table_put(info, oi, count);
 			rc = osd_remove_ois(info, osd);
 			if (rc)
 				GOTO(out, rc);
 
-			sf->sf_oi_count = osd_oi_count;
+			count = osd_oi_count;
 		}
 
 		scrub_file_reset(scrub, osd->od_uuid, SF_RECREATED);
-		count = sf->sf_oi_count;
+		sf->sf_oi_count = count;
 		goto create;
 	}
 