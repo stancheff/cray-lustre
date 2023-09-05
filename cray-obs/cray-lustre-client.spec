stancheff/cray-lustre

commit 0678731bcc7ac59af30df1e79055b5b2098543c4

LUS-11641 cray-obs: subproject cray-lustre-client-dkms
Incorporate the dkms build from cray-lustre-client-dkms.spec
into cray-lustre-client.spec directly.

This also updates some Requires: dependencies in
cray-lustre-client.spec and cray-lustre-client-ofed.spec
to work with either the kernel module packages or the dkms
installed package.

Update dkms scripts to pass kmoddir and auto detect
ofa_kernel and kfabric and pass the appropriate options
to configure.

Update installation of lustre and lnet headers.

Remove cray-lustre-client-dkms.spec as it was not used
and is no longer needed.

Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I2e535d0f6ab50f7549ae6e95bbd07420961e7920
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162402
Reviewed-by: Rajeev Mishra <rajeevm@hpe.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

cray-obs/cray-lustre-client.spec
@@ -10,6 +10,8 @@
 %define _sysconfdir /etc
 
 %global lustre_name cray-lustre-client
+%define module %{lustre_name}
+%define mkconf lustre/scripts/dkms.mkconf
 
 Name: %{lustre_name}
 Summary: Cray Lustre Filesystem
@@ -40,9 +42,10 @@ Requires: cray-kfabric-udev
 %global _with_linux --with-linux=/usr/src/kernels/%{kversion}
 %global requires_kmod_name kmod-%{lustre_name}
 %global requires_kmod_version %{version}
-Requires: kmod-cray-kfabric
-Requires: kmod-mlnx-ofa_kernel
+Requires: (kmod-cray-kfabric or cray-kfabric-dkms)
+Requires: (kmod-mlnx-ofa_kernel or mlnx-ofa_kernel-dkms)
 BuildRequires: redhat-rpm-config
+%define mkconf_options %{nil}
 %else
 %global kversion %(make -s -C /usr/src/linux-obj/%{_target_cpu}/%{flavor} kernelrelease)
 %global _with_linux --with-linux=/usr/src/linux
@@ -50,8 +53,9 @@ BuildRequires: redhat-rpm-config
 %global requires_kmod_name %{lustre_name}-kmp
 %global krequires %(echo %{kversion} | sed -e 's/\.x86_64$//' -e 's/\.i[3456]86$//' -e 's/-smp$//' -e 's/-bigsmp$//' -e 's/[-.]ppc64$//' -e 's/\.aarch64$//' -e 's/-default$//' -e 's/-%{flavor}//')
 %global requires_kmod_version %{version}_k%(echo %{krequires} | sed -r 'y/-/_/; s/^(2\.6\.[0-9]+)_/\\1.0_/;')
-Requires: cray-kfabric-kmp
-Requires: mlnx-ofa_kernel-kmp
+Requires: (cray-kfabric-kmp or cray-kfabric-dkms)
+Requires: (mlnx-ofa_kernel-kmp or mlnx-ofa_kernel-dkms)
+%define mkconf_options -k updates
 %endif
 
 Requires: %{requires_kmod_name} = %{requires_kmod_version}
@@ -104,6 +108,35 @@ Compiled for kernel: %{kversion}
 ko2iblnd compiled against: mlnx-ofa_kernel-devel-%{mofed_version}
 kkfilnd compiled against: cray-kfabric-devel-%{kfabric_version}
 
+%package dkms
+Group: System/Filesystems
+License: GPL
+Summary: Cray Lustre Filesystem DKMS
+URL: %url
+BuildArch: noarch
+Requires: dkms >= 2.2.0.3-28.git.7c3e7c5
+Requires: gcc, make, perl
+Requires: libtool libyaml-devel zlib-devel
+Requires: libnl3-devel keyutils-devel
+Requires: cray-kfabric-devel
+Requires: automake
+Requires: pkg-config
+Requires: kernel-devel
+Requires: autoconf
+Requires: bash-completion bash-completion-devel
+Requires: libmount-devel
+# NOTE: mlnx-ofa_kernel-devel appears to be optional
+Requires: flex
+Requires: bison
+Provides: %{lustre_name} = %{version}
+Conflicts:  kmod-%{lustre_name}
+Conflicts:  %{lustre_name}-kmp
+
+%description dkms
+This package contains the dkms Lustre kernel modules.
+and userspace tools and files for the Lustre filesystem.
+Compiled for kernel: %{kversion}
+
 %if %{undefined kmoddir}
 	%if %{defined kernel_module_package_moddir}
 		%global kmoddir %{kernel_module_package_moddir}
@@ -134,6 +167,16 @@ ln -f lnet/ChangeLog ChangeLog-lnet
 
 %build
 echo "LUSTRE_VERSION = %{_version}" > LUSTRE-VERSION-FILE
+
+# DKMS
+%{mkconf} -n %{module} -v %{version} -f dkms.conf %{mkconf_options}
+if [ "$RPM_BUILD_ROOT" != "/" ]; then
+    rm -rf $RPM_BUILD_ROOT
+fi
+mkdir -p $RPM_BUILD_ROOT/usr/src
+cp -rfp ${RPM_BUILD_DIR}/cray-lustre-%{version} $RPM_BUILD_ROOT/usr/src/
+mv $RPM_BUILD_ROOT/usr/src/cray-lustre-%{version} $RPM_BUILD_ROOT/usr/src/%{module}-%{version}
+# end DKMS
 
 if [ "%reconfigure" == "1" -o ! -x %_builddir/%{name}-%{version}/configure ];then
 	chmod +x autogen.sh
@@ -186,6 +229,8 @@ do
 	eval "sed -i 's,@cfgdir@,%{cfgdir},' cray-obs/${f}"
 	install -D -m 0644 cray-obs/${f} $RPM_BUILD_ROOT%{_pkgconfigdir}/${f}
 done
+# DKMS
+install -D -m 0644 cray-obs/cray-lnet.pc $RPM_BUILD_ROOT%{_pkgconfigdir}/cray-lnet-dkms.pc
 
 if [[ -e %{buildroot}/%{_sysconfdir}/modprobe.d/ko2iblnd.conf ]]; then
 	%{__sed} -i -e 's/^\(install ko2iblnd .*\)/\#\1/' %{buildroot}/%{_sysconfdir}/modprobe.d/ko2iblnd.conf
@@ -256,12 +301,48 @@ rm -f $RPM_BUILD_ROOT%{_libdir}/liblnetconfig.la
 %{_includedir}/interval_tree.h
 %{_pkgconfigdir}/cray-lnet.pc
 
+%files dkms
+%defattr(-,root,root)
+/usr/src/%{module}-%{version}
+%{_pkgconfigdir}/cray-lnet-dkms.pc
+
 %post
 /sbin/ldconfig
 %systemd_post lnet.service
 
+%post dkms
+if [ ! -f %{_pkgconfigdir}/cray-lnet.pc ] ; then
+    ln -s %{_pkgconfigdir}/cray-lnet-dkms.pc %{_pkgconfigdir}/cray-lnet.pc
+fi
+
+COMMON_POSTINT=/usr/libexec/dkms/common.postinst
+if ! -f ${COMMON_POSTINT} ; then
+    if -f /usr/lib/dkms/common.postinst; then
+        COMMON_POSTINT=/usr/lib/dkms/common.postinst
+    fi
+fi
+for POSTINST in ${COMMON_POSTINT}; do
+    if [ -f $POSTINST ]; then
+        $POSTINST %{module} %{version}
+        exit $?
+    fi
+    echo "WARNING: $POSTINST does not exist."
+done
+echo -e "ERROR: DKMS version is too old and %{module} was not"
+echo -e "built with legacy DKMS support."
+echo -e "You must either rebuild %{module} with legacy postinst"
+echo -e "support or upgrade DKMS to a more current version."
+exit 1
+
 %preun
 %systemd_preun lnet.service
+
+%preun dkms
+if [ -L %{_pkgconfigdir}/cray-lnet.pc ] ; then
+    rm %{_pkgconfigdir}/cray-lnet.pc
+fi
+dkms remove -m %{module} -v %{version} --all --rpm_safe_upgrade
+exit 0
 
 %postun
 /sbin/ldconfig
@@ -275,3 +356,6 @@ rm -f $RPM_BUILD_ROOT%{_libdir}/liblnetconfig.la
 
 %clean
 %clean_build_root
+if [ "$RPM_BUILD_ROOT" != "/" ]; then
+    rm -rf $RPM_BUILD_ROOT
+fi

.cray-obs-config.xml
@@ -74,11 +74,6 @@
             </file>
             <file>
                 <name>
-                    cray-obs/cray-lustre-client-dkms.spec
-                </name>
-            </file>
-            <file>
-                <name>
                     cray-obs/cray-lustre-client-ofed.spec
                 </name>
             </file>

cray-obs/cray-lustre-client-dkms.spec
@@ -1,146 +1,0 @@
-# LUTF Turn off brp-python-precompile script as we don't want the python files
-# to be compiled on installation
-%global __os_install_post %(echo '%{__os_install_post}' | sed -e 's!/usr/lib[^[:space:]]*/brp-python-bytecompile[[:space:]].*$!!g')
-
-%define _version %(if test -s "%_sourcedir/_version"; then cat "%_sourcedir/_version"; else echo "UNKNOWN"; fi)
-%define _lnet_version %(echo "%{_version}" | awk -F . '{printf("%s.%s", $1, $2)}')
-
-%define intranamespace_name %{name}
-%{expand:%%global OBS_prefix %{_prefix}}
-%define prefix /usr
-%define _sysconfdir /etc
-
-%global lustre_name cray-lustre-client
-%define module %{lustre_name}
-
-%define buildid 1
-%define mkconf  lustre/scripts/dkms.mkconf
-
-%bcond_with ofed
-
-%if 0%{with ofed}
-%define use_ofed 1
-%endif
-
-%if "%{_vendor}" == "redhat"
-%global kversion %(make -s -C /usr/src/kernels/* kernelversion)
-%global _with_linux --with-linux=/usr/src/kernels/%{kversion}
-%global requires_kmod_name kmod-%{lustre_name}
-%global requires_kmod_version %{version}
-%define mkconf_options %{nil}
-%define ofed_module_pkg kmod-mlnx-ofa_kernel
-%else
-%global kversion %(make -s -C /usr/src/linux-obj/%{_target_cpu}/%{flavor} kernelrelease)
-%global _with_linux --with-linux=/usr/src/linux
-%global _with_linux_obj --with-linux-obj=/usr/src/linux-obj/%{_target_cpu}/%{flavor}
-%global requires_kmod_name %{lustre_name}-kmp
-%global krequires %(echo %{kversion} | sed -e 's/\.x86_64$//' -e 's/\.i[3456]86$//' -e 's/-smp$//' -e 's/-bigsmp$//' -e 's/[-.]ppc64$//' -e 's/\.aarch64$//' -e 's/-default$//' -e 's/-%{flavor}//')
-%global requires_kmod_version %{version}_k%(echo %{krequires} | sed -r 'y/-/_/; s/^(2\.6\.[0-9]+)_/\\1.0_/;')
-%define mkconf_options -k updates
-%define ofed_module_pkg mlnx-ofa_kernel-kmp-%{flavor}
-%endif
-
-Name: %{module}-dkms
-Summary: Cray Lustre Filesystem
-Version: %{_version}
-Release: %{release}
-License: GPL
-Group: System/Filesystems
-Source: cray-lustre-%{_version}.tar.bz2
-Source1: kmp-lustre.preamble
-Source2: kmp-lustre.files
-URL: %url
-BuildRoot: %{_tmppath}/%{name}-%{version}-root
-BuildArch: noarch
-
-# Vendor specific requires/defines/etc.
-%if "%{_vendor}" == "redhat"
-BuildRequires: redhat-rpm-config
-%endif
-
-%if 0%{?use_ofed}
-BuildRequires: mlnx-ofa_kernel-devel
-Requires: %{ofed_module_pkg}
-%else
-Requires: cray-gni-devel
-Requires: cray-gni-headers
-Requires: cray-gni-headers-private
-Requires: cray-krca-devel
-Requires: %{namespace}-krca-devel
-%endif
-
-Requires: dkms >= 2.2.0.3-28.git.7c3e7c5
-Requires: gcc, make, perl
-Requires: libtool libyaml-devel zlib-devel
-Requires: libnl3-devel keyutils-devel
-Requires: cray-kfabric-devel
-Requires: automake
-Requires: pkg-config
-Requires: kernel-devel
-Requires: autoconf
-Requires: bash-completion bash-completion-devel
-Requires: libmount-devel
-
-BuildRequires: %kernel_module_package_buildreqs
-BuildRequires: libtool libyaml-devel zlib-devel
-BuildRequires: systemd
-BuildRequires: libnl3-devel
-BuildRequires: keyutils-devel
-BuildRequires: flex
-BuildRequires: bison
-
-Provides: %{lustre_name} = %{version}
-Provides: %{requires_kmod_name} = %{requires_kmod_version}
-
-# Disable post-build-checks; See LUS-1345
-# Note: build checks can be run manually by first doing an incremental build
-# and then doing a second incremental build with post-build-checks enabled.
-#!BuildIgnore: post-build-checks
-
-%description
-This package contains the dkms Lustre kernel modules.
-and userspace tools and files for the Lustre filesystem.
-Compiled for kernel: %{kversion}
-
-%prep
-%setup -q -n cray-lustre-%{version}
-
-%build
-echo "LUSTRE_VERSION = %{_version}" > LUSTRE-VERSION-FILE
-%{mkconf} -n %{module} -v %{version} -f dkms.conf %{mkconf_options}
-
-%install
-if [ "$RPM_BUILD_ROOT" != "/" ]; then
-    rm -rf $RPM_BUILD_ROOT
-fi
-mkdir -p $RPM_BUILD_ROOT/usr/src/
-cp -rfp ${RPM_BUILD_DIR}/cray-lustre-%{version} $RPM_BUILD_ROOT/usr/src/
-mv $RPM_BUILD_ROOT/usr/src/cray-lustre-%{version} $RPM_BUILD_ROOT/usr/src/%{module}-%{version}
-
-%clean
-if [ "$RPM_BUILD_ROOT" != "/" ]; then
-    rm -rf $RPM_BUILD_ROOT
-fi
-
-%files
-%defattr(-,root,root)
-/usr/src/%{module}-%{version}
-
-%post
-for POSTINST in /usr/lib/dkms/common.postinst \
-		/usr/libexec/dkms/common.postinst; do
-    if [ -f $POSTINST ]; then
-        $POSTINST %{module} %{version}
-        exit $?
-    fi
-    echo "WARNING: $POSTINST does not exist."
-done
-echo -e "ERROR: DKMS version is too old and %{module} was not"
-echo -e "built with legacy DKMS support."
-echo -e "You must either rebuild %{module} with legacy postinst"
-echo -e "support or upgrade DKMS to a more current version."
-exit 1
-
-%preun
-dkms remove -m %{module} -v %{version} --all --rpm_safe_upgrade
-exit 0

cray-obs/cray-lustre-client-ofed.spec
@@ -30,7 +30,7 @@ BuildRequires: libmount-devel
 BuildRequires: cray-kfabric-devel
 BuildRequires: flex
 BuildRequires: bison
-Requires: cray-kfabric-udev
+Requires: (cray-kfabric-udev or cray-kfabric-dkms)
 
 # Vendor specific requires/defines/etc.
 %if %{_vendor}=="redhat"
@@ -38,7 +38,7 @@ Requires: cray-kfabric-udev
 %global _with_linux --with-linux=/usr/src/kernels/%{kversion}
 %global requires_kmod_name kmod-%{lustre_name}
 %global requires_kmod_version %{version}
-Requires: kmod-cray-kfabric
+Requires: (kmod-cray-kfabric or cray-kfabric-dkms)
 BuildRequires: redhat-rpm-config
 %else
 %global kversion %(make -s -C /usr/src/linux-obj/%{_target_cpu}/%{flavor} kernelrelease)
@@ -47,7 +47,7 @@ BuildRequires: redhat-rpm-config
 %global requires_kmod_name %{lustre_name}-kmp
 %global krequires %(echo %{kversion} | sed -e 's/\.x86_64$//' -e 's/\.i[3456]86$//' -e 's/-smp$//' -e 's/-bigsmp$//' -e 's/[-.]ppc64$//' -e 's/\.aarch64$//' -e 's/-default$//' -e 's/-%{flavor}//')
 %global requires_kmod_version %{version}_k%(echo %{krequires} | sed -r 'y/-/_/; s/^(2\.6\.[0-9]+)_/\\1.0_/;')
-Requires: cray-kfabric-kmp
+Requires: (cray-kfabric-kmp or cray-kfabric-dkms)
 %endif
 
 Requires: %{requires_kmod_name} = %{requires_kmod_version}
@@ -61,7 +61,7 @@ Requires: %{requires_kmod_name} = %{requires_kmod_version}
 Userspace tools and files for the Lustre filesystem.
 Compiled for kernel: %{kversion}
 ko2iblnd compiled against: In-kernel drivers
-kkfilnd compiled against: cray-kfabric-devel-%{kfabric_version}
+kkfilnd compiled against: cray-kfabric-devel-%{kfabric_version} or cray-kfabric-dkms
 
 %package devel
 Group: Development/Libraries

lustre-dkms_post-build.sh
@@ -6,6 +6,7 @@
 # $5 : $arch
 # $6 : $source_tree
 # $7 : $dkms_tree
+# $8 : $kmoddir
 #
 # This script ensure that ALL Lustre kernel modules that have been built
 # during DKMS build step of lustre[-client]-dkms module will be moved in
@@ -34,7 +35,8 @@ case $1 in
 		      vdev_clear-lustre.sh \
 		      vdev_remove-lustre.sh
 	do
-		install -D -m 0755 lustre/scripts/${script} /etc/zfs/zed.d/${script}
+	    install -D -m 0755 lustre/scripts/${script} /etc/zfs/zed.d/${script}
+	    >&2 echo "installing lustre/scripts/${script} => /etc/zfs/zed.d/${script}"
 	done
 	;;
 
@@ -52,11 +54,14 @@ case $1 in
 	do
 	    target=$(echo ${fname} | sed -e 's:^lnet/include/::g')
 	    if [[ ${target} == uapi/* ]]; then
-	        header=$(echo ${fname} | sed -e 's:^uapi/linux/lnet/::g')
+	        header=$(echo ${target} | sed -e 's:^uapi/linux/lnet/::g')
 	        install -D -m 0644 ${fname} /usr/include/uapi/linux/lnet/${header}
 	        install -D -m 0644 ${fname} /usr/include/linux/lnet/${header}
+	        >&2 echo "installing ${fname} => /usr/include/uapi/linux/lnet/${header}"
+	        >&2 echo "installing ${fname} => /usr/include/linux/lnet/${header}"
 	    else
-	        install -D -m 0644 $fname} /usr/include/${target}
+	        install -D -m 0644 ${fname} /usr/include/${target}
+	        >&2 echo "installing ${fname} => /usr/include/${target}"
 	    fi
 	done
 
@@ -65,9 +70,11 @@ case $1 in
 	do
 	    target=$(echo ${fname} | sed -e 's:^libcfs/:/usr/:g')
 	    install -D -m 0644 ${fname} ${target}
+	    >&2 echo "installing ${fname} => ${target}"
 	done
 
 	install -D -m 0644 lustre/include/interval_tree.h /usr/include/interval_tree.h
+	>&2 echo "installing lustre/include/interval_tree.h => /usr/include/interval_tree.h"
 
 	cfgdir=/usr/include/lustre/${flavor}
 	_version=$2
@@ -83,6 +90,7 @@ case $1 in
 	    eval "sed -i 's,@PACKAGE_VERSION@,${_version},' cray-obs/${f}"
 	    eval "sed -i 's,@cfgdir@,${cfgdir},' cray-obs/${f}"
 	    install -D -m 0644 cray-obs/${f} /usr/lib64/pkgconfig/${f}
+	    >&2 echo "installing cray-obs/${f} => /usr/lib64/pkgconfig/${f}"
 	done
 	;;
 esac

lustre-dkms_pre-build.sh
@@ -6,12 +6,28 @@
 # $5 : $arch
 # $6 : $source_tree
 # $7 : $dkms_tree
+# $8 : $kmoddir [cray-lustre-client either 'extra|updates']
 
 case $1 in
     cray-lustre-client)
 	SERVER="--disable-server --enable-client"
 	ksrc="$(dirname $4)/source"
 	KERNEL_STUFF="--with-linux=$(realpath $ksrc) --with-linux-obj=$(realpath $4)"
+	name=$1
+	kmoddir=$8
+	flavor=$(echo $3 | tr '-' '\n' | tail -1)
+	if [ -L /usr/src/kfabric ]; then
+		KERNEL_STUFF="${KERNEL_STUFF} --with-kfi=/usr/src/kfabric/${flavor}"
+	fi
+	if [ -d /usr/src/ofa_kernel/${flavor} ]; then
+		O2IBPATH=/usr/src/ofa_kernel/${flavor}
+	elif [ -d /usr/src/ofa_kernel/default ]; then
+		O2IBPATH=/usr/src/ofa_kernel/default
+	else
+		O2IBPATH=yes
+	fi
+	KERNEL_STUFF="${KERNEL_STUFF} --with-o2ib=${O2IBPATH}"
+	KERNEL_STUFF="${KERNEL_STUFF} --with-kmp-moddir=${kmoddir}/${name}"
 	sh ./autogen.sh
 	;;
 

lustre/scripts/dkms.mkconf
@@ -25,10 +25,10 @@ PACKAGE_CONFIG="/etc/sysconfig/lustre"
 # re-evaluate/create dkms.conf on-target based on local build-time products/dependencies
 # (like krb5-devel presence) and also upon configure results/errors before dkms builds
 PRE_BUILD="lustre-dkms_pre-build.sh \$module \$module_version \$kernelver \$kernel_source_dir \
-	 \$arch \$source_tree \$dkms_tree"
+	 \$arch \$source_tree \$dkms_tree \$kmoddir"
 # to ensure all just built Lustre kernel modules will be saved for further install
 POST_BUILD="lustre-dkms_post-build.sh \$module \$module_version \$kernelver \$kernel_source_dir \
-	 \$arch \$source_tree \$dkms_tree"
+	 \$arch \$source_tree \$dkms_tree \$kmoddir"
 EOF
 if [ $pkgname = "lustre-zfs" -o $pkgname = "lustre-all" ] ; then
 	cat >>${filename} <<EOF

commit e6ec38c2fd557a245f00b1c70ffaa33265f7afcf

LUS-11800 build: Use brackets "[]" around sh conditionals
Correctly place logical conditional tests within [] brackets
to resolve a syntax error that fails with:

  Checking for file conflicts: .........................[done]
  /var/tmp/rpm-tmp.bTJFgI: line 6: -f: command not found
  /var/tmp/rpm-tmp.bTJFgI: line 7: -f: command not found
  WARNING: /usr/libexec/dkms/common.postinst does not exist.
  ERROR: DKMS version is too old and cray-lustre-client was not
  built with legacy DKMS support.

Fixes: 0678731bcc ("LUS-11641 cray-obs: subproject cray-lustre-client-dkms")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I0db7763777373c43271dda5e16eaa53ae75e205f
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162501
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

cray-obs/cray-lustre-client.spec
@@ -326,8 +326,8 @@ if [ ! -f %{_pkgconfigdir}/cray-lnet.pc ] ; then
 fi
 
 COMMON_POSTINT=/usr/libexec/dkms/common.postinst
-if ! -f ${COMMON_POSTINT} ; then
-    if -f /usr/lib/dkms/common.postinst; then
+if [ ! -f ${COMMON_POSTINT} ] ; then
+    if [ -f /usr/lib/dkms/common.postinst ] ; then
         COMMON_POSTINT=/usr/lib/dkms/common.postinst
     fi
 fi