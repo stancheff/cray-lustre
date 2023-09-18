Cray/lustre

commit 62dbfa9494157514aa659f1f60b5446a4dce7d2f

LUS-11265 lod: improve rr allocation
Roundrobin allocation uses atomic_inc() % ost_count for
generation OST index. When some OSTs are unavailable and
many threads make object creation, it could happen that
OST idx is the same for all attempts. For example with
4 OSTs configuration when 2 OSTs do faiover, estimation
of probability is 0.5^12=0.024%. The result is ENOSPC for
user application.

Let's try one by one OSTs for a last speed loop.

Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
Change-Id: If960f23780548dd652947db77d54f23d3c304d21
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162362
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lustre/lod/lod_qos.c
@@ -749,7 +749,7 @@ static int lod_ost_alloc_rr(const struct lu_env *env, struct lod_object *lo,
 	__u32 stripe_idx = 0;
 	__u32 stripe_count, stripe_count_min, ost_idx;
 	int rc, speed = 0, ost_connecting = 0;
-	int stripes_per_ost = 1;
+	int idx, stripes_per_ost = 1;
 	bool overstriped = false;
 	ENTRY;
 
@@ -808,11 +808,13 @@ repeat_find:
 		  lqr->lqr_start_count, lqr->lqr_offset_idx, osts->op_count,
 		  osts->op_count);
 
-	for (i = 0; i < osts->op_count * stripes_per_ost &&
+	for (i = 0, idx = 0; i < osts->op_count * stripes_per_ost &&
 		    stripe_idx < stripe_count; i++) {
-		int idx;
+		if (likely(speed < 2) || i == 0)
+			idx = atomic_inc_return(&lqr->lqr_start_idx);
+		else
+			idx++;
 
-		idx = atomic_inc_return(&lqr->lqr_start_idx);
 		array_idx = (idx + lqr->lqr_offset_idx) %
 				osts->op_count;
 		ost_idx = lqr->lqr_pool.op_array[array_idx];

stancheff/cray-lustre

commit 028513df5ff6ce9e67f1fa016c0054e6837984f0

LUS-11265 lod: accurate OSTs iteration for the last speed
The first fix removed lqr->lqr_start_idx from calculation
of ost_idx for the last speed loop. It was not enough
because lqr->lqr_offset_idx is used at calculation and
also could be changed by another thread. And it leads
to the same OST index during a loop.

1694009994: lod_ost_alloc_rr()) #0 strt 2166 act 0 strp 0 ary 0 idx 0
1694010094: lod_ost_alloc_rr()) #1 strt 2167 act 0 strp 0 ary 0 idx 0
1694010197: lod_ost_alloc_rr()) rc -28

Fixes: 62dbfa9494 ("LUS-11265 lod: improve rr allocation")
Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
Change-Id: I9a415afd9894182ffd0cf3d7aa40f6464c054d3e
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162514
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

lustre/lod/lod_qos.c
@@ -797,13 +797,16 @@ repeat_find:
 
 	for (i = 0, idx = 0; i < osts->op_count * stripes_per_ost &&
 		    stripe_idx < stripe_count; i++) {
-		if (likely(speed < 2) || i == 0)
-			idx = atomic_inc_return(&lqr->lqr_start_idx);
-		else
+		if (likely(speed < 2) || i == 0) {
+			idx = atomic_inc_return(&lqr->lqr_start_idx) +
+			      lqr->lqr_offset_idx;;
+		} else {
+			/*
+			 * For last speed, use OSTs one by one
+			 */
 			idx++;
-
-		array_idx = (idx + lqr->lqr_offset_idx) %
-				osts->op_count;
+		}
+		array_idx = idx % osts->op_count;
 		ost_idx = lqr->lqr_pool.op_array[array_idx];
 
 		QOS_DEBUG("#%d strt %d act %d strp %d ary %d idx %d\n",