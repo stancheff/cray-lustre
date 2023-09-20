stancheff/cray-lustre

commit c7409a1129b9852a0efb431cf31ce27c390d7e76

LUS-11560 llite: use right predicate in ll_atomic_open
Using d_unhashed() brings a race window with d_add() and d_drop()
leading to dentry hash table corruption. If dentry which is in hash
already is added to hash table, it gets looped to itself via next
pointer:
	dentry 0xffff8fd34cc08840
	...
		d_hash = {
    		       next = 0xffff8fd34cc08848,
    		       pprev = 0x0
  		},

See for reference:
commit 00699ad8571afd7fb8bc2c61f67c86c2428680ab
Author: Al Viro <viro@zeniv.linux.org.uk>
Date:   Tue Jul 5 09:44:53 2016 -0400

    Use the right predicate in ->atomic_open() instances

Signed-off-by: Vladimir Saveliev <vladimir.saveliev@hpe.com>
Change-Id: I6c27f031d0d7e7d571752d6172a32406ad68e913
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162401
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

lustre/llite/namei.c
@@ -1204,14 +1204,14 @@ static int ll_atomic_open(struct inode *dir, struct dentry *dentry,
 	/* Only negative dentries enter here */
 	LASSERT(dentry->d_inode == NULL);
 
-	if (!d_unhashed(dentry)) {
+	if (!d_in_lookup(dentry)) {
 		/* A valid negative dentry that just passed revalidation,
 		 * there's little point to try and open it server-side,
 		 * even though there's a minuscule chance it might succeed.
 		 * Either way it's a valid race to just return -ENOENT here.
 		 */
 		if (!(open_flags & O_CREAT))
-			return -ENOENT;
+			RETURN(-ENOENT);
 
 		/* Otherwise we just unhash it to be rehashed afresh via
 		 * lookup if necessary

commit 9e369728d3951c9d77a2b321853562492c504920

LUS-11823 llite: fix ll_atomic_open for kernel 3.10
Linux kernel version 3.10 does not provide d_in_lookup(), Lustre
defines it as return false which is ok for ll_dcompare() but not for
ll_atomic_open().

Keep using d_unhashed() in ll_atomic_open() for kernels without
d_in_lookup().

Change-Id: Ia528c40bf1c525fc86ecc038dea84b312b7f9817
Signed-off-by: Vladimir Saveliev <vladimir.saveliev@hpe.com>
Fixes: c7409a1129 (LUS-11560 llite: use right predicate in ll_atomic_open)
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162515
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

lustre/llite/namei.c
@@ -1188,7 +1188,11 @@ static int ll_atomic_open(struct inode *dir, struct dentry *dentry,
 	/* Only negative dentries enter here */
 	LASSERT(dentry->d_inode == NULL);
 
+#ifndef HAVE_D_IN_LOOKUP
+	if (!d_unhashed(dentry)) {
+#else
 	if (!d_in_lookup(dentry)) {
+#endif
 		/* A valid negative dentry that just passed revalidation,
 		 * there's little point to try and open it server-side,
 		 * even though there's a minuscule chance it might succeed.