Cray/lustre

commit e267a7c8be91b99252ba64a6b7f08f6b34604c3b

LUS-10924 libcfs: Ensure libcfs_set_so_reuseport is available
Move libcfs_set_so_reuseport out of libcfs_set_so_reuseport

Fixes: a48d4bd8d5 (LUS-10353 libcfs: sk_reuseport not avail use kernel_setsockopt")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Iebbb1b5eb6d6d60c44e82b068d911171d79a2848
Reviewed-on: https://es-gerrit.dev.cray.com/160571
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

libcfs/include/libcfs/linux/linux-net.h
@@ -99,11 +99,6 @@ lnet_genl_dumpit_info(struct netlink_callback *cb)
 }
 #endif /* HAVE_GENL_DUMPIT_INFO */
 
-#ifdef HAVE_KERNEL_SETSOCKOPT
-
-#include <net/tcp.h>
-#include <net/sock.h>
-
 static inline void libcfs_set_so_reuseport(struct socket *sock, int option)
 {
 #ifndef HAVE_SOCKET_SK_SK_REUSEPORT
@@ -113,6 +108,11 @@ static inline void libcfs_set_so_reuseport(struct socket *sock, int option)
 	sock->sk->sk_reuseport = option;
 #endif
 }
+
+#ifdef HAVE_KERNEL_SETSOCKOPT
+
+#include <net/tcp.h>
+#include <net/sock.h>
 
 #if !defined(HAVE_TCP_SOCK_SET_QUICKACK)
 static inline void tcp_sock_set_quickack(struct sock *sk, int opt)

stancheff/cray-lustre

commit 277dda2d3c793ad6d78b8b23387cbe3a157b921e

LUS-11801 libcfs: include sock.h for HAVE_SOCKET_SK_SK_REUSEPORT
linux-net.h currently only includes net/sock.h if
HAVE_KERNEL_SETSOCKOPT is defined. libcfs_set_so_reuseport() can
reference struct sock so we also need to include net/sock.h before its
definition when HAVE_SOCKET_SK_SK_REUSEPORT is defined. Otherwise, the
build can fail due to incomplete type.

Test-Parameters: trivial
Fixes: e267a7c8be ("LUS-10924 libcfs: Ensure libcfs_set_so_reuseport is available")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Ib867580d72d4ad26afbb7001434dc7d84158d77d
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162503
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

libcfs/include/libcfs/linux/linux-net.h
@@ -25,6 +25,9 @@
 
 #include <net/netlink.h>
 #include <net/genetlink.h>
+#if defined(HAVE_SOCKET_SK_SK_REUSEPORT) || defined(HAVE_KERNEL_SETSOCKOPT)
+#include <net/sock.h>
+#endif
 
 #ifndef HAVE_NLA_STRDUP
 char *nla_strdup(const struct nlattr *nla, gfp_t flags);
@@ -116,7 +119,6 @@ static inline void libcfs_set_so_reuseport(struct socket *sock, int option)
 #ifdef HAVE_KERNEL_SETSOCKOPT
 
 #include <net/tcp.h>
-#include <net/sock.h>
 
 #if !defined(HAVE_TCP_SOCK_SET_QUICKACK)
 static inline void tcp_sock_set_quickack(struct sock *sk, int opt)