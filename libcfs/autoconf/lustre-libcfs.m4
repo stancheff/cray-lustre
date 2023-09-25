stancheff/cray-lustre

commit abd763bd3e484765c63dccb26615b8e70172a747

LUS-11559 osc: Prefer NR_ZONE_WRITE_PENDING
Linux commit v4.7-5966-g5a1c84b404a7
 mm: remove reclaim and compaction retry approximations

Introduced NR_ZONE_WRITE_PENDING which should be used
in mod_zone_page_state.

Older kernels should fallback to NR_WRITEBACK.

Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Idb4c3a25beae021a87f215368094c59513d40de2
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162392
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

libcfs/autoconf/lustre-libcfs.m4
@@ -928,6 +928,28 @@ AC_DEFUN([LIBCFS_RHASHTABLE_INSERT_FAST], [
 ]) # LIBCFS_RHASHTABLE_INSERT_FAST
 
 #
+# LIBCFS_HAVE_NR_ZONE_WRITE_PENDING
+#
+# kernel v4.7-5966-g5a1c84b404a7
+# mm: remove reclaim and compaction retry approximations
+#
+AC_DEFUN([LIBCFS_SRC_HAVE_NR_ZONE_WRITE_PENDING], [
+	LB2_LINUX_TEST_SRC([nr_zone_write_pending_exists], [
+		#include <linux/mmzone.h>
+	],[
+		enum zone_stat_item item = NR_ZONE_WRITE_PENDING;
+		(void)item;
+	],[-Werror])
+])
+AC_DEFUN([LIBCFS_HAVE_NR_ZONE_WRITE_PENDING], [
+	AC_MSG_CHECKING([if NR_ZONE_WRITE_PENDING is available])
+	LB2_LINUX_TEST_RESULT([nr_zone_write_pending_exists], [
+		AC_DEFINE(HAVE_NR_ZONE_WRITE_PENDING, 1,
+			[NR_ZONE_WRITE_PENDING is still in use.])
+	])
+]) # LIBCFS_HAVE_NR_ZONE_WRITE_PENDING
+
+#
 # Kernel version 4.7-rc1 commit 8f6fd83c6c5ec66a4a70c728535ddcdfef4f3697
 # added 3rd arg to rhashtable_walk_init
 #
@@ -1961,29 +1983,6 @@ AC_DEFUN([LIBCFS_VMALLOC_2ARGS], [
 ]) # LIBCFS_VMALLOC_2ARGS
 
 #
-# LIBCFS_HAVE_NR_UNSTABLE_NFS
-#
-# kernel v5.8-rc1~201^2~75
-# mm/writeback: discard NR_UNSTABLE_NFS, use NR_WRITEBACK instead
-#
-AC_DEFUN([LIBCFS_SRC_HAVE_NR_UNSTABLE_NFS], [
-	LB2_LINUX_TEST_SRC([nr_unstable_nfs_exists], [
-		#include <linux/mm.h>
-
-		int i;
-	],[
-		i = NR_UNSTABLE_NFS;
-	],[-Werror])
-])
-AC_DEFUN([LIBCFS_HAVE_NR_UNSTABLE_NFS], [
-	AC_MSG_CHECKING([if NR_UNSTABLE_NFS still in use])
-	LB2_LINUX_TEST_RESULT([nr_unstable_nfs_exists], [
-		AC_DEFINE(HAVE_NR_UNSTABLE_NFS, 1,
-			[NR_UNSTABLE_NFS is still in use.])
-	])
-]) # LIBCFS_HAVE_NR_UNSTABLE_NFS
-
-#
 # LIBCFS_HAVE_MMAP_LOCK
 #
 # kernel v5.8-rc1~83^2~24
@@ -2446,6 +2445,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_SRC], [
 	LIBCFS_SRC_RHASHTABLE_LOOKUP
 	LIBCFS_SRC_RHLTABLE
 	LIBCFS_SRC_STACKTRACE_OPS
+	LIBCFS_SRC_HAVE_NR_ZONE_WRITE_PENDING
 	# 4.9
 	LIBCFS_SRC_GET_USER_PAGES_GUP_FLAGS
 	LIBCFS_SRC_RHASHTABLE_WALK_ENTER
@@ -2503,7 +2503,6 @@ AC_DEFUN([LIBCFS_PROG_LINUX_SRC], [
 	LIBCFS_SRC_TCP_SOCK_SET_KEEPINTVL
 	LIBCFS_SRC_TCP_SOCK_SET_KEEPCNT
 	# 5.8
-	LIBCFS_SRC_HAVE_NR_UNSTABLE_NFS
 	LIBCFS_SRC_HAVE_MMAP_LOCK
 	LIBCFS_SRC_KERNEL_SETSOCKOPT
 	LIBCFS_SRC_VMALLOC_2ARGS
@@ -2596,6 +2595,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_RESULTS], [
 	LIBCFS_RHASHTABLE_LOOKUP
 	LIBCFS_RHLTABLE
 	LIBCFS_STACKTRACE_OPS
+	LIBCFS_HAVE_NR_ZONE_WRITE_PENDING
 	# 4.9
 	LIBCFS_GET_USER_PAGES_GUP_FLAGS
 	LIBCFS_RHASHTABLE_WALK_ENTER
@@ -2653,7 +2653,6 @@ AC_DEFUN([LIBCFS_PROG_LINUX_RESULTS], [
 	LIBCFS_TCP_SOCK_SET_KEEPINTVL
 	LIBCFS_TCP_SOCK_SET_KEEPCNT
 	# 5.8
-	LIBCFS_HAVE_NR_UNSTABLE_NFS
 	LIBCFS_HAVE_MMAP_LOCK
 	LIBCFS_KERNEL_SETSOCKOPT
 	LIBCFS_VMALLOC_2ARGS

lustre/osc/osc_page.c
@@ -894,17 +894,8 @@ void osc_lru_unreserve(struct client_obd *cli, unsigned long npages)
 	wake_up(&osc_lru_waitq);
 }
 
-/**
- * Atomic operations are expensive. We accumulate the accounting for the
- * same page zone to get better performance.
- * In practice this can work pretty good because the pages in the same RPC
- * are likely from the same page zone.
- */
-#ifdef HAVE_NR_UNSTABLE_NFS
-/* Old kernels use a separate counter for unstable pages,
- * newer kernels treat them like any other writeback.
- */
-#define NR_WRITEBACK NR_UNSTABLE_NFS
+#ifndef HAVE_NR_ZONE_WRITE_PENDING
+#define NR_ZONE_WRITE_PENDING NR_WRITEBACK
 #endif
 
 static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
@@ -935,7 +926,7 @@ static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
 		}
 
 		if (count > 0) {
-			mod_zone_page_state(zone, NR_WRITEBACK,
+			mod_zone_page_state(zone, NR_ZONE_WRITE_PENDING,
 					    factor * count);
 			count = 0;
 		}
@@ -943,7 +934,7 @@ static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
 		++count;
 	}
 	if (count > 0)
-		mod_zone_page_state(zone, NR_WRITEBACK, factor * count);
+		mod_zone_page_state(zone, NR_ZONE_WRITE_PENDING, factor * count);
 }
 
 static inline void add_unstable_page_accounting(struct ptlrpc_bulk_desc *desc,

commit 76481de7a1b789338ffac9f07a35b7d4b8a04be2

LUS-11559 osc: Prefer NR_ZONE_WRITE_PENDING to NR_UNSTABLE_NFS
Linux commit v4.7-5966-g5a1c84b404a7
 mm: remove reclaim and compaction retry approximations

Introduced NR_ZONE_WRITE_PENDING which should be used
in mod_zone_page_state.

Older kernels should fallback to NR_UNSTABLE_NFS
or NR_WRITEBACK.

Fixes: abd763bd3e ("LUS-11559 osc: Prefer NR_ZONE_WRITE_PENDING")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I951584610987faf3173d40eb3ddf77bc9448c7f5
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162488
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

libcfs/autoconf/lustre-libcfs.m4
@@ -2452,6 +2452,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_SRC], [
 	LIBCFS_SRC_RHASHTABLE_INSERT_FAST
 	LIBCFS_SRC_RHASHTABLE_WALK_INIT_3ARG
 	# 4.8
+	LIBCFS_SRC_HAVE_NR_ZONE_WRITE_PENDING
 	LIBCFS_SRC_RHASHTABLE_LOOKUP
 	LIBCFS_SRC_RHLTABLE
 	LIBCFS_SRC_STACKTRACE_OPS
@@ -2602,6 +2603,7 @@ AC_DEFUN([LIBCFS_PROG_LINUX_RESULTS], [
 	LIBCFS_RHASHTABLE_INSERT_FAST
 	LIBCFS_RHASHTABLE_WALK_INIT_3ARG
 	# 4.8
+	LIBCFS_HAVE_NR_ZONE_WRITE_PENDING
 	LIBCFS_RHASHTABLE_LOOKUP
 	LIBCFS_RHLTABLE
 	LIBCFS_STACKTRACE_OPS

lustre/osc/osc_page.c
@@ -894,8 +894,20 @@ void osc_lru_unreserve(struct client_obd *cli, unsigned long npages)
 	wake_up(&osc_lru_waitq);
 }
 
-#ifndef HAVE_NR_ZONE_WRITE_PENDING
-#define NR_ZONE_WRITE_PENDING NR_WRITEBACK
+/**
+ * Atomic operations are expensive. We accumulate the accounting for the
+ * same page zone to get better performance.
+ * In practice this can work pretty good because the pages in the same RPC
+ * are likely from the same page zone.
+ */
+#ifdef HAVE_NR_UNSTABLE_NFS
+/* Old kernels use a separate counter for unstable pages,
+ * newer kernels treat them like any other writeback.
+ * (see Linux commit: v5.7-467-g8d92890bd6b8)
+ */
+#define NR_ZONE_WRITE_PENDING		((enum zone_stat_item)NR_UNSTABLE_NFS)
+#elif !defined(HAVE_NR_ZONE_WRITE_PENDING)
+#define NR_ZONE_WRITE_PENDING		((enum zone_stat_item)NR_WRITEBACK)
 #endif
 
 static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
@@ -934,7 +946,8 @@ static inline void unstable_page_accounting(struct ptlrpc_bulk_desc *desc,
 		++count;
 	}
 	if (count > 0)
-		mod_zone_page_state(zone, NR_ZONE_WRITE_PENDING, factor * count);
+		mod_zone_page_state(zone, NR_ZONE_WRITE_PENDING,
+				    factor * count);
 }
 
 static inline void add_unstable_page_accounting(struct ptlrpc_bulk_desc *desc,